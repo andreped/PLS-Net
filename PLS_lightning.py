677fddb7b4d1b3e051e52b8771b2630b6e127eb8
André Pedersen <andrped94@gmail.com>
2023-02-13 11:11:46 +0100

    made pytorch PLS Net implementation - refactored

--- PLS_lightning.py
+++ PLS_lightning.py
@@ -61,22 +61,22 @@ class PLS(pl.LightningModule):
         # ENCODER
         # l = 1
         x = x
-        input = x
+        input_ = x
         out = self.ds_conv_1(x)
-        downsampled_1 = F.interpolate(input, scale_factor=0.5, mode='trilinear', align_corners=False)
+        downsampled_1 = F.interpolate(input_, scale_factor=0.5, mode='trilinear', align_corners=False)
         out = torch.cat([out, downsampled_1], 1)
         out_l1 = self.drdb_1(out)
 
         # l = 2
         out = self.ds_conv_2(out_l1)
-        downsampled_2 = F.interpolate(input, scale_factor=0.25, mode='trilinear', align_corners=False)
+        downsampled_2 = F.interpolate(input_, scale_factor=0.25, mode='trilinear', align_corners=False)
         out = torch.cat([out, downsampled_2], 1)
         out = self.drdb_2_1(out)
         out_l2 = self.drdb_2_2(out)
 
         # l = 3
         out = self.ds_conv_3(out_l2)
-        downsampled_3 = F.interpolate(input, scale_factor=0.125, mode='trilinear', align_corners=False)
+        downsampled_3 = F.interpolate(input_, scale_factor=0.125, mode='trilinear', align_corners=False)
         out = torch.cat([out, downsampled_3], 1)
         out = self.drdb_3_1(out)
         out = self.drdb_3_2(out)
@@ -143,56 +143,3 @@ class PLS(pl.LightningModule):
         parser.add_argument('--n_channels', type=int, default=1)
         parser.add_argument('--n_classes', type=int, default=2)
         return parser
-
-
-def flatten(tensor):
-    """Flattens a given tensor such that the channel axis is first.
-    The shapes are transformed as follows:
-       (N, C, D, H, W) -> (C, N * D * H * W)
-    """
-    # number of channels
-    C = tensor.size(1)
-    # new axis order
-    axis_order = (1, 0) + tuple(range(2, tensor.dim()))
-    # Transpose: (N, C, D, H, W) -> (C, N, D, H, W)
-    transposed = tensor.permute(axis_order)
-    # Flatten: (C, N, D, H, W) -> (C, N * D * H * W)
-    return transposed.contiguous().view(C, -1)
-
-
-def compute_per_channel_dice(input, target, epsilon=1e-6, weight=None, exclude_background=True):
-    """
-    Computes DiceCoefficient as defined in https://arxiv.org/abs/1606.04797 given  a multi channel input and target.
-    Assumes the input is a normalized probability, e.g. a result of Sigmoid or Softmax function.
-
-    Args:
-         input (torch.Tensor): NxCxSpatial input tensor
-         target (torch.Tensor): NxCxSpatial target tensor
-         epsilon (float): prevents division by zero
-         weight (torch.Tensor): Cx1 tensor of weight per channel/class
-    """
-
-    # input and target shapes must match
-    assert input.size() == target.size(), "'input' and 'target' must have the same shape"
-
-    if not exclude_background:
-        input = flatten(input[:, 0:, ...])
-        target = flatten(target[:, 0:, ...])
-    else:
-        input = flatten(input[:, 1:, ...])
-        target = flatten(target[:, 1:, ...])
-    target = target.float()
-
-    # compute per channel Dice Coefficient
-    intersect = (input * target).sum(-1)
-    if weight is not None:
-        intersect = weight * intersect
-
-    # here we can use standard dice (input + target).sum(-1) or extension (see V-Net) (input^2 + target^2).sum(-1)
-    denominator = (input * input).sum(-1) + (target * target).sum(-1)
-    loss_per_channel = 2 * (intersect / denominator.clamp(min=epsilon))
-    return loss_per_channel.sum() / loss_per_channel.size(0)
-
-
-def compute_dice_loss(input, target, epsilon=1e-6, weight=None):
-    return 1 - compute_per_channel_dice(input, target)
---
+++ PLS_pytorch.py
@@ -0,0 +1,79 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from torch import optim
+from PLS_buildingblocks import DSConv3D, DrdbBlock3D, DecoderBlock
+
+
+class PLS_pytorch(nn.Module):
+    def __init__(self, nb_classes: int=2):
+        super(PLS_pytorch, self).__init__()
+        self.nb_classes = nb_classes
+
+        # Network specific arguments
+        self.nb_channels = [0, 16, 64, 128]
+        self.growth_rate = 12
+
+        # ENCODER
+        self.ds_conv_1 = DSConv3D(self.nb_channels[0] + 1, self.nb_channels[1])
+        self.drdb_1 = DrdbBlock3D(self.nb_channels[1] + 1, self.nb_channels[1] + 1, self.growth_rate)
+
+        self.ds_conv_2 = DSConv3D(self.nb_channels[1] + 1, self.nb_channels[2])
+        self.drdb_2_1 = DrdbBlock3D(self.nb_channels[2] + 1, self.nb_channels[2] + 1, self.growth_rate)
+        self.drdb_2_2 = DrdbBlock3D(self.nb_channels[2] + 1, self.nb_channels[2] + 1, self.growth_rate)
+
+        self.ds_conv_3 = DSConv3D(self.nb_channels[2] + 1, self.nb_channels[3])
+        self.drdb_3_1 = DrdbBlock3D(self.nb_channels[3] + 1, self.nb_channels[3] + 1, self.growth_rate)
+        self.drdb_3_2 = DrdbBlock3D(self.nb_channels[3] + 1, self.nb_channels[3] + 1, self.growth_rate)
+        self.drdb_3_3 = DrdbBlock3D(self.nb_channels[3] + 1, self.nb_channels[3] + 1, self.growth_rate)
+        self.drdb_3_4 = DrdbBlock3D(self.nb_channels[3] + 1, self.nb_channels[3] + 1, self.growth_rate)
+
+        # DECODER
+        self.ds_bridge_l2 = DSConv3D(in_chans=self.nb_channels[2] + 1, out_chans=self.nb_classes * 2, dstride=1)
+        self.ds_bridge_l1 = DSConv3D(in_chans=self.nb_channels[1] + 1, out_chans=self.nb_classes * 2, dstride=1)
+
+        self.decoder_l3 = DecoderBlock(in_chans=self.nb_channels[-1] + 1, out_chans=self.nb_classes * 2)
+        self.decoder_l2 = DecoderBlock(in_chans=self.nb_classes * 4, out_chans=self.nb_classes * 2)
+        self.decoder_l1 = DecoderBlock(in_chans=self.nb_classes * 4, out_chans=self.nb_classes * 2)
+
+        # OUTPUT
+        self.decoder_l0 = nn.Conv3d(in_channels=self.nb_classes * 2, out_channels=self.nb_classes, kernel_size=1)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        # ENCODER
+        # l = 1
+        x = x
+        input_ = x
+        out = self.ds_conv_1(x)
+        downsampled_1 = F.interpolate(input_, scale_factor=0.5, mode='trilinear', align_corners=False)
+        out = torch.cat([out, downsampled_1], 1)
+        out_l1 = self.drdb_1(out)
+
+        # l = 2
+        out = self.ds_conv_2(out_l1)
+        downsampled_2 = F.interpolate(input_, scale_factor=0.25, mode='trilinear', align_corners=False)
+        out = torch.cat([out, downsampled_2], 1)
+        out = self.drdb_2_1(out)
+        out_l2 = self.drdb_2_2(out)
+
+        # l = 3
+        out = self.ds_conv_3(out_l2)
+        downsampled_3 = F.interpolate(input_, scale_factor=0.125, mode='trilinear', align_corners=False)
+        out = torch.cat([out, downsampled_3], 1)
+        out = self.drdb_3_1(out)
+        out = self.drdb_3_2(out)
+        out = self.drdb_3_3(out)
+        out = self.drdb_3_4(out)
+
+        # DECODER
+        out = self.decoder_l3(out)
+        out = torch.cat([out, self.ds_bridge_l2(out_l2)], 1)
+        out = self.decoder_l2(out)
+        out = torch.cat([out, self.ds_bridge_l1(out_l1)], 1)
+        out = self.decoder_l1(out)
+        out = self.decoder_l0(out)
+        out = self.softmax(out)
+
+        return out
