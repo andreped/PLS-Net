f2676d40b29481d9e1021610693d9b6f4b685161
André Pedersen <andrped94@gmail.com>
2023-02-13 11:12:00 +0100

    moved stuff to utils

---
+++ utils.py
@@ -0,0 +1,54 @@
+import torch
+
+
+def flatten(tensor):
+    """Flattens a given tensor such that the channel axis is first.
+    The shapes are transformed as follows:
+       (N, C, D, H, W) -> (C, N * D * H * W)
+    """
+    # number of channels
+    C = tensor.size(1)
+    # new axis order
+    axis_order = (1, 0) + tuple(range(2, tensor.dim()))
+    # Transpose: (N, C, D, H, W) -> (C, N, D, H, W)
+    transposed = tensor.permute(axis_order)
+    # Flatten: (C, N, D, H, W) -> (C, N * D * H * W)
+    return transposed.contiguous().view(C, -1)
+
+
+def compute_per_channel_dice(input, target, epsilon=1e-6, weight=None, exclude_background=True):
+    """
+    Computes DiceCoefficient as defined in https://arxiv.org/abs/1606.04797 given  a multi channel input and target.
+    Assumes the input is a normalized probability, e.g. a result of Sigmoid or Softmax function.
+
+    Args:
+         input (torch.Tensor): NxCxSpatial input tensor
+         target (torch.Tensor): NxCxSpatial target tensor
+         epsilon (float): prevents division by zero
+         weight (torch.Tensor): Cx1 tensor of weight per channel/class
+    """
+
+    # input and target shapes must match
+    assert input.size() == target.size(), "'input' and 'target' must have the same shape"
+
+    if not exclude_background:
+        input = flatten(input[:, 0:, ...])
+        target = flatten(target[:, 0:, ...])
+    else:
+        input = flatten(input[:, 1:, ...])
+        target = flatten(target[:, 1:, ...])
+    target = target.float()
+
+    # compute per channel Dice Coefficient
+    intersect = (input * target).sum(-1)
+    if weight is not None:
+        intersect = weight * intersect
+
+    # here we can use standard dice (input + target).sum(-1) or extension (see V-Net) (input^2 + target^2).sum(-1)
+    denominator = (input * input).sum(-1) + (target * target).sum(-1)
+    loss_per_channel = 2 * (intersect / denominator.clamp(min=epsilon))
+    return loss_per_channel.sum() / loss_per_channel.size(0)
+
+
+def compute_dice_loss(input, target, epsilon=1e-6, weight=None):
+    return 1 - compute_per_channel_dice(input, target)
